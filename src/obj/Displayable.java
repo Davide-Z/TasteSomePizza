02053964a64c010883bfd36a960c0e41e89431b3
ASUS N56VZ <ASUS N56VZ@DESKTOP-1UJ93OP>
2017-02-23 15:24:42 +0100

    Displayable - actualMap, createNewId()

--- src/obj/Displayable.java
+++ src/obj/Displayable.java
@@ -1,10 +1,43 @@
 package obj;
-  
+
+import maps.Vec;
+import maps.Map;
+import maps.Case;
+import maps.Mat;
+
+
 public abstract class Displayable {
 	abstract void appear();
 	abstract void disappear();
-	//Vec pos();
+	Vec pos;
 	int id;
 	String type;
-
+	int lastId=0; // sert pour créer les identifiants uniques
+	Map actualMap;
+	
+	Displayable(String t, Vec p){
+		id=createNewId();
+		type=t;
+		pos=p;
+	}
+	
+	int createNewId(){
+		return ++lastId;
+	}
+	
+	public Vec getPos() {
+		return pos;
+	}
+	public void setPos(Vec pos) {
+		this.pos = pos;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
 }
